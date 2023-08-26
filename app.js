d66e2e467ad8acff301a7adeb4a35f5991f8b871
MOONUJ <141498537+MOONUJ@users.noreply.github.com>
2023-08-26 22:49:26 +0900

    update login form

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
-const logInInput = document.querySelector(".login-form input");
-const logInBtn = document.querySelector(".login-form button");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const loginBtn = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
 
-function handleLoginBtnClick() {
-    const username = logInInput.value;
-    if(username === "") {
-        alert("Please write your name.");
-    } else if(username.length > 15) {
-        alert("Your name is too long.")
-    }
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
 }
 
-logInBtn.addEventListener("click", handleLoginBtnClick);
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null) {
+     //show the form
+     loginForm.classList.remove(HIDDEN_CLASSNAME);
+     loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    //show the h1
+    paintGreetings(savedUsername);
+}
