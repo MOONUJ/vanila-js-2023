62cce14d081096fe9d9dc7d0d9e5cf7c4c651ff6
MOONUJ <141498537+MOONUJ@users.noreply.github.com>
2023-08-29 22:50:51 +0900

    login-form & main page style update

--- js/greetings.js
+++ js/greetings.js
@@ -2,12 +2,15 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const loginBtn = document.querySelector("#login-form button");
 const greeting = document.querySelector("#greeting");
+const appearElement = document.getElementsByClassName("afterLoginAppear");
 
 const HIDDEN_CLASSNAME = "hidden";
+const FLEX_CLASSNAME = "flex";
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
+    loginForm.classList.remove(FLEX_CLASSNAME);
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
     localStorage.setItem(USERNAME_KEY, username);
@@ -15,8 +18,15 @@ function onLoginSubmit(event) {
 }
 
 function paintGreetings(username) {
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello! ${username}`;
+    paintAppearElement(appearElement.length);
+}
+
+function paintAppearElement(length) {
+    for(i=0; i < length; i++){
+        appearElement[i].classList.add(FLEX_CLASSNAME);
+        appearElement[i].classList.remove(HIDDEN_CLASSNAME);
+    }
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
@@ -26,6 +36,7 @@ const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if(savedUsername === null) {
      //show the form
+     loginForm.classList.add(FLEX_CLASSNAME);
      loginForm.classList.remove(HIDDEN_CLASSNAME);
      loginForm.addEventListener("submit", onLoginSubmit);
 } else {
--- js/weather.js
+++ js/weather.js
@@ -5,8 +5,8 @@ function onGeoOk(position) {
     console.log(position);
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url).then(response => response.json()).then(data => {
-        const weather = document.querySelector("#weather span:first-child");
-        const city = document.querySelector("#weather span:last-child");
+        const weather = document.querySelector("#weather span:last-child");
+        const city = document.querySelector("#weather span:first-child");
         city.innerText = data.name;
         weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
     });
